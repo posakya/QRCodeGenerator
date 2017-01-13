6ad0aa42305de38351145ff3fc95237a6d3ce6c4
posakya <roshanposakya07@gmail.com>
2017-01-13 23:18:35 +0545

    QR code Generator a Desktop Application

---
+++ src/qrcode/QRcode.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qrcode;
+
+
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import javax.imageio.ImageIO;
+//
+//import com.google.zxing.BarcodeFormat;
+//import com.google.zxing.BinaryBitmap;
+//import com.google.zxing.EncodeHintType;
+//import com.google.zxing.MultiFormatReader;
+//import com.google.zxing.MultiFormatWriter;
+//import com.google.zxing.NotFoundException;
+//import com.google.zxing.Result;
+//import com.google.zxing.WriterException;
+//import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+//import com.google.zxing.client.j2se.MatrixToImageWriter;
+//import com.google.zxing.common.BitMatrix;
+//import com.google.zxing.common.HybridBinarizer;
+//import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+
+/**
+ *
+ * @author roshan
+ */
+
+  
+public class QRcode {
+
+	public static void main(String[] args) {
+            Maker make=new Maker();
+            make.setVisible(true);
+	}
+}
